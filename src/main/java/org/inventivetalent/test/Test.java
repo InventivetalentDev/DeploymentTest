4f196fec59a53ad445416989f8138ac038923bec
inventivetalent <inventivetalent@inventivegames.de>
2016-06-19 17:03:15 +0200

    Initial commit

---
+++ src/main/java/org/inventivetalent/test/Test.java
@@ -0,0 +1,13 @@
+package org.inventivetalent.test;
+
+public class Test {
+
+	public void uselessMethod() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void anotherUselessMethod() {
+		throw new UnsupportedOperationException("Stop calling me!");
+	}
+
+}
